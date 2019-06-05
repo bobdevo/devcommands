2e639924973080f1748bb98e6d56b9c61e009aa4
Roberto Martins <roberto.martins.info@gmail.com>
2019-06-05 10:17:22 -0300

    Atualização

--- main.sh
+++ main.sh
@@ -8,11 +8,39 @@ dmssh(){
     sshfs $1@$2:/$3 $4
 }
 
+dhtml(){
+	echo "Do you really know what you're doing? (y) or (n)."
+	read choice
+	echo    # (optional) move to a new line
+	if [[ $choice =~ ^[Yy]$ ]]
+	then
+	    # do dangerous stuff
+
+	    mkdir ./css
+	    mkdir ./images
+	    mkdir ./javascript
+	    mkdir ./partials
+	    mkdir ./fonts
+	    mkdir ./pages
+	    mkdir ./layout
+	    touch ./css/style.css
+	    touch ./javascript/script.js
+	    touch ./index.php
+	    touch ./partials/head.php
+	    touch ./partials/header.php
+	    touch ./partials/footer.php
+	    touch ./layout/default.php
+	    touch ./pages/home.php
+  	  	# touch ./pages/_contact.php
+  	fi
+
+}
+
 d(){
-	eval "$1"		
+	eval "$1"
 }
 dnot(){
-	notify-send $1 
+	notify-send $1
 }
 dmkcd(){
 	mkdir -p -- "$1" &&
@@ -26,7 +54,7 @@ dhosts(){
 dgitinit(){
 	mkdir -p -- "$1" &&
 	cd -P -- "$1" &&
-	git init 		
+	git init
 }
 
 dopen(){
@@ -38,30 +66,30 @@ dphp-s(){
 }
 
 dtmux(){
-	tmux new-session -d  
+	tmux new-session -d
     tmux split-window -h
     tmux split-window -v 'alsamixer'
-    tmux split-window -h 
+    tmux split-window -h
     tmux split-window -v
     tmux resize-pane -D 2
-    tmux clock-mode  
+    tmux clock-mode
     tmux resize-pane -L 4
     tmux resize-pane -U 2
     tmux select-pane -t 3
     tmux split-window -v 'htop'
     tmux resize-pane -U 4
-    tmux select-pane -t 1 
+    tmux select-pane -t 1
     tmux split-window -v
-    tmux -2 attach -d 
+    tmux -2 attach -d
 }
 dtheme(){
-    /opt/oomox/gui.sh 
-}  
+    /opt/oomox/gui.sh
+}
 dlog(){
-	case $1 in 
+	case $1 in
 		"apache")
-			tail -f /var/log/apache2/error.log	
-	esac	
+			tail -f /var/log/apache2/error.log
+	esac
 }
 dnote(){
 	echo "$1" >> ./log
@@ -91,6 +119,19 @@ dstat(){
     git status
 }
 dproject(){
-    [! -d "~/subl-project/" ] && mkdir -p -- "~/subl-project"
-    subl --command "close_project"; subl --project ~/subl-project/$1.sublime-project
+
+
+
+    if test ! -d ~/subl-project
+    then
+        mkdir ~/subl-project
+    fi
+
+    if test ! -d ~/subl-project/$1.sublime-project
+    then
+        echo "{}" > ~/subl-project/$1.sublime-project
+    else
+        subl --command "close_project"; subl --project ~/subl-project/$1.sublime-project
+    fi
+
 }
